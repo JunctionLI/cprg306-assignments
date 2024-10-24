cb787d8960f65ee9ec8b161762a99d0a6310242f
lunian16 <lijunxianjunction@gmail.com>
2024-10-24 01:04:20 -0600

    week 8 assignment - api

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,8 +1,12 @@
-export default function Item({item}){
+
+
+
+
+export default function Item({item, onSelect}){
 
 
     return(
-        <div className="p-2 m-4 bg-yellow-100 max-w-sm">
+        <div className="p-2 m-4 bg-yellow-100 max-w-sm" onClick={() => onSelect(item)}>
             <h2 className="text-xl font-bold">{item.name}</h2>
             <p className="text-sm">Buy {item.quantity} in {item.category} </p>
         </div>
--- app/week-8/page.js
+++ app/week-8/page.js
@@ -14,6 +14,8 @@ export default function Week7Page(){
         ItemsData.map( (item) =>({...item}) )
     );
     const[sortBy, setSortBy] = useState("name");
+    const [selectedItemName, setSelectedItemName] = useState(""); 
+
 
     //function to add new item into list
     const handleAddItem = (newItem) => {
@@ -28,6 +30,17 @@ export default function Week7Page(){
     };
 
 
+
+    //clear name
+    const handleItemSelect = (item) => {
+      let cleanedName = item.name.split(",")[0].trim();
+      cleanedName = cleanedName.replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      );
+      setSelectedItemName(cleanedName);
+      console.log(`select items ${cleanedName}`);
+    };
 
 
   // Sort by name
@@ -48,10 +61,15 @@ export default function Week7Page(){
     return(
 
         <main className="flex">
+          {/**left part */}
           <div className="w-1/2 p-4">
             <h1 className="text-3xl font-bold m-2 text-yellow-500 ">Shopping List</h1>
             <NewItem onAddItem={handleAddItem}/>
-            <ItemList items={itemsList} handleSortByCategory={() => handleSortByCategory(itemsList)} handleSortByName={() => handleSortByName(itemsList)}/>
+            <ItemList items={itemsList} handleSortByCategory={() => handleSortByCategory(itemsList)} handleSortByName={() => handleSortByName(itemsList)} onItemSelect={handleItemSelect}/>
+          </div>
+          {/**right part: meal ideas*/}
+          <div className="w-1/2 p-4">
+            <MealIdeas ingredient={selectedItemName}/>
           </div>
 
             <Link href="./" className="italic hover:underline hover:text-green-400">Back to Main</Link>
